dc32475e2e88b62fe1e5a231a1fcfb0a1a65b4ab
hosu-kim <hosu@outlook.cz>
2026-03-20 05:14:45 +0100

    add leaveUser(), getMemberCount()

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -7,7 +7,7 @@ TODOS:
 		3. channel modes: i(invite-only), t(topic limit), k(password needed), l(user limit)
 	
 	# methods implementation
-		1. Join-leave: joinUser(User* user); leaveUser(User* user);
+		1. Join-leave: [Done]joinUser(User* user, std::string password); leaveUser(User* user);
 		2. permission control(add=remove): addOperator(User* user), removeOperator(User* user);
 		3. message broadcase: broadcase(std::string msg, User* exceptUser)
 			except the sender, all Users receive the messages.
@@ -32,6 +32,7 @@ TODOS:
 #include <poll.h>
 #include <signal.h>
 #include <map> // std::map
+#include <set>
 
 #include "User.hpp"
 
@@ -46,11 +47,15 @@ class Channel {
 		//======================================================================
 		/* Member Functions */
 		int joinUser(User* user, std::string password);
+		int leaveUser(User* user);
+		size_t getMemberCount() const;
 
 	private:
 		std::string _channelName;
 		std::string _channelPassword;
 		std::string _channelTopic;
+		// // for the channel mode `l`(user limit)
+		// if _userLimit is 0, unlimited user entry allowed
 		int			_userLimit;
 		/* I could use std::vector also, but when we search and kick a specific user,
 		   it's not effetive than std::map
@@ -58,4 +63,10 @@ class Channel {
 		     2. To find duplicate users, every time we have to uterate through them 😢 */
 		//       vv: nickname vv: user instance
 		std::map<std::string, User*> _channelMembers;
+		/* Key features of std::set
+		   1. ***Duplicates not allowed***: even if the same value is entered twice,
+		                                    only one is stored.
+		   2. Sorts elements in ascending order
+		*/
+		std::set<std::string> _channelOperators;
 };
--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,11 +1,13 @@
 #include "../include/Channel.hpp"
 
-/* Orthodox Canonical Form */
+/* ======================== Orthodox Canonical Form ========================= */
 Channel::Channel()
-	: _channelName("default"), _channelPassword("default"), _channelTopic("default"), _userLimit(0) {}
+	: _channelName("default"), _channelPassword("default"),
+	 _channelTopic("default"), _userLimit(0) {}
 
 Channel::Channel(std::string channelName, std::string channelPassword, std::string channelTopic, int userLimit)
-	: _channelName(channelName), _channelPassword(channelPassword), _channelTopic(channelTopic), _userLimit(userLimit) {}
+	: _channelName(channelName), _channelPassword(channelPassword),
+	_channelTopic(channelTopic), _userLimit(userLimit) {}
 
 Channel::Channel(const Channel& src) {
 	*this = src;
@@ -26,18 +28,47 @@ Channel& Channel::operator=(const Channel& src) {
 }
 
 Channel::~Channel() {}
-/*==============================================================================*/
 
-int joinUser(User* user, std::string password) {
-	if (this->_channelPassword != "" && this->_channelPassword != password)
-		return 475;
+/* ============================ Member Functions ============================ */
+// RFC standards error code used:
+// https://www.rfc-editor.org/rfc/rfc2812.html
+// 475(ERR_BADCHANNELKEY): Wrong password for the channel
+// 471(ERR_CHANNELISFULL): User limit reached
+// 0: Success to join a user
+int Channel::joinUser(User* user, std::string password) {
+	// 1. if user already exists in the channel
+	if (_channelMembers.find(user->getNickname()) != _channelMembers.end())
+		return 0; // Success
 
-	if (this->_channelMemebers.size() >= _userLimit) return 471;
+	// 2. password validation (only when +k[password needed] mode is enabled)
+	if (!this->_channelPassword.empty() && this->_channelPassword != password)
+		return 475; // ERR_BADCHANNELKEY
 
-	if (_channelMembers.find(user->getNickname()) != _channelMembers.end()) {
-		return 0;
-	}
+	// 3. user limit check (only when +l[num of user limited] mode is enabled)
+	// if _userLimit is 0, unlimited user entry allowed
+	if (this->_userLimit > 0 && this->_channelMembers.size() >= (size_t)_userLimit)
+		return 471;
 
+	// 4. User entry approval
 	_channelMembers[user->getNickname()] = user;
 	return 0; // Success
+}
+
+int Channel::leaveUser(User* user) {
+	// 1. user exists in the channel
+	std::string nickname = user->getNickname();
+	if (_channelMembers.find(nickname) == _channelMembers.end())
+		return 442; // ERR_NOTONCHANNEL
+
+	// 2. Deletes user in the channel members
+	_channelMembers.erase(nickname);
+
+	// 3. Deletes user in the channel operaters
+	_channelOperators.erase(nickname);
+
+	return 0;
+}
+
+size_t Channel::getMemberCount() const {
+	return _channelMembers.size();
 }
