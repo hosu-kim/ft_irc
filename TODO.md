2c6d0fcda70cca1341993d17658526ef536dbabf
hosu-kim <hosu@outlook.cz>
2026-05-13 16:56:00 +0200

    finish KICK command

--- TODO.md
+++ TODO.md
@@ -9,14 +9,14 @@
 	│   │   ├── CmdJOIN.hpp [DONE]
 	│   │   ├── CmdKICK.hpp [DONE] 08.05.2026
 	│   │   ├── CmdMODE.hpp [DONE] 08.05.2026
-	│   │   ├── CmdNICK.hpp [Working] 12.05.2026
+	│   │   ├── CmdNICK.hpp [Working] 12.05.2026 => [DONE] 13.05.2026
 	│   │   ├── CmdPASS.hpp [DONE]
 	│   │   ├── CmdPING.hpp [DONE] 12.05.2026
 	│   │   ├── CmdPRIMSG.hpp
 	│   │   ├── CmdQUIT.hpp
 	│   │   ├── CmdTOPIC.hpp
 	│   │   └── CmdUSER.hpp
-	│   ├── Server.hpp [DONE] => But some code blocks need to be moved to the Cmd classes - hosu [23.03.26]
+	│   ├── Server.hpp [DONE] => But some code blocks need to be moved to the Cmd classes - hosu (23.03.26)
 	│   └── User.hpp [DONE]
 	├── Makefile [DONE]
 	└── src
@@ -24,13 +24,12 @@
 	    ├── CmdFactory.cpp
 	    ├── commands
 	    │   ├── ACmd.cpp [DONE]
-	    │   ├── CmdINVITE.cpp
+	    │   ├── CmdINVITE.cpp [DONE]
 	    │   ├── CmdJOIN.cpp [DONE]
-	    │   ├── CmdKICK.cpp
-	    │   ├── CmdMODE.cpp
-	    │   ├── CmdNICK.cpp
-	    │   ├── CmdPASS.cpp
-	    │   ├── CmdPING.cpp
+	    │   ├── CmdKICK.cpp [Working] 12.05.2026 => [DONE] 13.05.2026
+	    │   ├── CmdMODE.cpp [DONE] 08.05.2026
+	    │   ├── CmdPASS.cpp [DONE]
+	    │   ├── CmdPING.cpp [DONE] 12.05.2026
 	    │   ├── CmdPRIMSG.cpp
 	    │   ├── CmdQUIT.cpp
 	    │   ├── CmdTOPIC.cpp
--- src/commands/CmdNICK.cpp
+++ src/commands/CmdNICK.cpp
@@ -7,7 +7,7 @@ void CmdNick::execute(User &user, Server &server)
 	*/
 	if (getParamCount() != 1)
 	{
-		std::string msg = server.getServerName() + " 431 " + server.getUserName() + " :Nickname not given. Please provide us with your nickname.";
+		std::string msg = ":" + server.getServerName() + " 431 " + server.getUserName() + " :Nickname not given. Please provide us with your nickname.";
 		user.reply(msg);
 		return;
 	}
@@ -15,16 +15,16 @@ void CmdNick::execute(User &user, Server &server)
 	/*
 	2. Get the requested nickname.
 	*/
-	std::string nick = getParam(1);
+	std::string requested_nick = getParam(0);
 	
 	/*
 	3. Check for invalid characters in the nickname
 	*/
 	// find_first_of: - returns the index of the first character that matches any character in the given string
 	//                - C++98 standard library function(<string>)
-	size_t pos = nick.find_first_of(FORBIDDEN_CHARS);
+	size_t pos = requested_nick.find_first_of(FORBIDDEN_CHARS);
 	if (pos != std::string::npos) {
-		std::string msg = server.getServerName() + " 432 " + server.getUserName() + " :invalid nickname";
+		std::string msg = ":" + server.getServerName() + " 432 " + server.getUserName() + " :invalid nickname";
 		user.reply(msg);
 		return;
 	}
@@ -32,49 +32,24 @@ void CmdNick::execute(User &user, Server &server)
 	/*
 	4. Check if the nickname is already taken by someone else in the server
 	*/
-	else 
-	{
-		//set nickname
-		user.setNickname(nick);
-		user.setHasNick(true);
-
-		std::string temp1 = "Nickname set successfully. \r\n";
-		std::cout << temp1 << std::endl << "NicknameOK is: " << user.getHasNick() << std::endl;
-		std::cout << "Nickname is: " << user.getNickname() << std::endl;
+	// // Using _params[i] directly could cause a segfault if the requested index doesn't exist. => Use getParam()
+	if (server.findUserByNick(getParam(0))) {
+		std::string current_nick = user.getNickname().empty() ? "*" : user.getNickname();
+		std::string msg = ":" + server.getServerName() + " 433 " + current_nick + " " + requested_nick + " :nickname already exists in the server";
+		user.reply(msg);
+		return;
 	}
-	std::string nickMsg = ": user " + user.getUserName() + " NICK " + user.getNickname() + " @ " + user.getHostName() + "\r\n";
-	// send()
+
+	/*
+	5. Success! Apply the new nickname
+	*/
+	std::string old_nick = user.getNickname();
+	user.setNickname(getParam(0));
+	user.setHasNick(true);
+
+	/*
+	6. Announce the NICK change
+	*/
+	std::string success_msg = ":" + old_nick + "!" + user.getUserName() + "@" + user.getHostName() + " NICK :" + user.getNickname();
+	user.reply(success_msg);
 }
-
-/*
-FUNCTION execute(user, server):
-
-    // 1. Check if the nickname parameter is missing
-    IF parameter count is NOT equal to 1:
-        SEND ERR_NONICKNAMEGIVEN (431) to user
-        RETURN
-        
-    // 2. Get the requested nickname (It's the first parameter, not a channel name!)
-    SET requested_nickname = GET parameter at index 0
-    
-    // 3. Check for invalid characters in the nickname
-    FOR EACH character IN requested_nickname:
-        IF character is in FORBIDDEN_CHARS:
-            SEND ERR_ERRONEUSNICKNAME (432) to user
-            RETURN // Stop here if it's invalid!
-            
-    // 4. Check if the nickname is already taken by someone else in the server
-    IF server.isNicknameInUse(requested_nickname) is TRUE:
-        SEND ERR_NICKNAMEINUSE (433) to user
-        RETURN // Stop here if it's taken!
-        
-    // 5. Success! Apply the new nickname
-    SET user's nickname to requested_nickname
-    SET user's 'hasNick' status to TRUE
-    
-    // 6. Announce the NICK change
-    FORMAT success_message = ":<old_nick/user> NICK <new_nick>"
-    SEND success_message to the user (and others who need to know)
-
-END FUNCTION
-*/
